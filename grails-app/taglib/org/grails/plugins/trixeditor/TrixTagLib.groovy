619ae07a05b60074f6e25056fe6cf8566f30350a
sdelamo <sergio.delamo@softamo.com>
2017-02-03 15:01:35 +0100

    Initial plugin

---
+++ grails-app/taglib/org/grails/plugins/trixeditor/TrixTagLib.groovy
@@ -0,0 +1,17 @@
+package org.grails.plugins.trixeditor
+
+class TrixTagLib {
+    static namespace = "trix" // <1>
+    static defaultEncodeAs = [taglib:'text'] // <2>
+
+    def editor = { attrs, body ->
+        def id = attrs.id ?: attrs.name
+
+        out << "<input id=\"${id}\" type=\"hidden\" name=\"${attrs.name}\""
+        if ( attrs.value ) {
+            out << " value=\"${attrs.value.encodeAsHTML()}\""  // <2>
+        }
+        out << " />"
+        out << "<trix-editor input=\"${id}\"></trix-editor>"
+    }
+}

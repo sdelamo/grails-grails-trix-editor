21f1528c4e10d240431277721b231e679c66d3e8
sdelamo <sergio.delamo@softamo.com>
2017-03-03 12:03:14 +0100

    Don’t publish snapshots to OJO

--- travis-build.sh
+++ travis-build.sh
@@ -9,9 +9,6 @@ if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST
   if [[ -n $TRAVIS_TAG ]]; then
     echo "Pushing build to Bintray"
     ./gradlew bintrayUpload || EXIT_STATUS=$?
-  else
-    echo "Publishing snapshot to OJO"
-    ./gradlew artifactoryPublish || EXIT_STATUS=$?
   fi
 fi
 exit $EXIT_STATUS
